97a8182193374b27e35195ed45e4a39a8af779d4
Alina M <crlin.m@gmail.com>
2024-11-06 18:01:48 +0500

    feat: +2 task (46/49)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -480,8 +480,8 @@ function getHexRGBValues(arr) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  return arr.sort((a, b) => b - a).slice(0, n);
 }
 
 /**
@@ -496,8 +496,21 @@ function getMaxItems(/* arr, n */) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const dd = arr2.reduce((acc, item) => {
+    acc[item] = acc[item] + 1 || 1;
+    return acc;
+  }, {});
+  return arr1.reduce((acc, item) => {
+    if (item in dd) {
+      dd[item] -= 1;
+      if (dd[item] === 0) {
+        delete dd[item];
+      }
+      acc.push(item);
+    }
+    return acc;
+  }, []);
 }
 
 /**
